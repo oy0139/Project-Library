ef838e0507f391b97623fd41b2d2f5591df7c060
Roy Lopez <150288003+oy0139@users.noreply.github.com>
2025-08-09 19:57:32 -0400

    Complete Part 1 to manage a collection of Book Objects

---
+++ main.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(author, title, numOfPages, isRead) {
+    this.author = author;
+    this.title = title;
+    this.numOfPages = numOfPages;
+    this.isRead = isRead;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary (author, title, numOfPages, isRead) {
+    let newObject = new Book(author, title, numOfPages, isRead)
+    myLibrary.push(newObject);
+}
